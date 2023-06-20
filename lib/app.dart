5fded8a46fe91bb190a092506f93cfad06838546
Bruno Coelho <brunoca2000@hotmail.com>
2023-06-20 12:40:32 -0300

    fix: languageController locale in main.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:pomodoro_timer/core/routes/routes.dart';
+import 'package:pomodoro_timer/features/controllers/language_controller.dart';
 import 'package:pomodoro_timer/features/controllers/pomodoro_controller.dart';
 import 'package:pomodoro_timer/features/controllers/settings_controller.dart';
+import 'package:pomodoro_timer/core/localization/multi_languages.dart';
 import 'package:provider/provider.dart';
 
 void application() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const Application());
 }
 
@@ -17,6 +22,7 @@ class Application extends StatefulWidget {
 
 class _ApplicationState extends State<Application> {
   final _appRouter = Routes();
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -29,10 +35,34 @@ class _ApplicationState extends State<Application> {
             Provider.of<PomodoroController>(context, listen: false),
           ),
         ),
+        ChangeNotifierProvider<LanguageController>(
+          create: (context) => LanguageController(),
+        ),
       ],
-      child: MaterialApp.router(
-        routerConfig: _appRouter.config(),
-        debugShowCheckedModeBanner: false,
+      child: Builder(
+        builder: (context) {
+          LanguageController langController =
+              Provider.of<LanguageController>(context);
+
+          return MaterialApp.router(
+            routerConfig: _appRouter.config(),
+            debugShowCheckedModeBanner: false,
+            supportedLocales: const [
+              Locale('en', 'US'),
+              Locale.fromSubtags(languageCode: 'pt'),
+            ],
+            localizationsDelegates: const [
+              MultiLanguagesImpl.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            locale: langController.locale,
+            localeResolutionCallback: (locale, supported) {
+              return langController.localeResolutionCallback(locale, supported);
+            },
+          );
+        },
       ),
     );
   }
